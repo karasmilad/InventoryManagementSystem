47ba7ccf3fe6b2bd5716c9f880fafc3fe5fd4d4f
karasmilad <elpopkaras12@gmail.com>
2026-08-15 01:11:25 +0300

    Add UnitOfWork

---
+++ InventoryManagementSystem.Domain/Contracts/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using InventoryManagementSystem.Domain.Entities;
+
+namespace InventoryManagementSystem.Domain.Contracts
+{
+    public interface IUnitOfWork
+    {
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+        IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>() where TEntity : BaseEntity<TKey>;
+    }
+}
---
+++ InventoryManagementSystem.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using InventoryManagementSystem.Domain.Contracts;
+using InventoryManagementSystem.Domain.Entities;
+using InventoryManagementSystem.Infrastructure.Data;
+
+namespace InventoryManagementSystem.Infrastructure.Repositories
+{
+    public class UnitOfWork(StoreDbContext dbContext) : IUnitOfWork
+    {
+        private readonly Dictionary<string, object> repositories = [];
+        public IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>() where TEntity : BaseEntity<TKey>
+        {
+            var typeName = typeof(TEntity).Name;
+            if (repositories.TryGetValue(typeName,out object? value))
+            {
+                return (IGenericRepository<TEntity,TKey>)value;
+            }
+            else
+            {
+                var repo = new GenericRepository<TEntity, TKey>(dbContext);
+                repositories[typeName] = repo;
+                return repo;
+            }
+                
+        }
+        public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return await dbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
